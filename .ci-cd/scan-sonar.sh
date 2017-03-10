308dec8114d8a1100630293bc31f69f0215bce0f
Swindle, Gregory Jay (Greg) <gregory.jay.swindle@one.verizon.com>
2017-03-10 17:31:06 -0600

    chore(sonar-scanner): remove local installation

--- .ci-cd/scan-sonar.sh
+++
@@ -1,6 +0,0 @@
-~/local/bin/sonar-scanner \
-    -Dsonar.projectKey=0c37bbd4b50b5bdff38bd7fd9eeb6265094a684d \
-    -Dsonar.sources=. \
-    -Dsonar.host.url=https://sonarqube.com \
-    -Dsonar.organization=gregswindle-github \
-    -Dsonar.login=gregswindle@github

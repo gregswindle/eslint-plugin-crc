9aac38ad940b4235a1e1a5dac9ed33d3acbe9068
Greg Swindle <greg@swindle.net>
2017-04-19 22:14:47 -0500

    chore(ci service pruning): remove redundant ci services (#30)
    * test(cli): add specs for cli
    
    #1
    
    * refactor(cli): rename command-line interface
    
    * Continuously run specs/tests with nodemon
    * Generate ESLint reports
    
    #5
    
    * style(lint): enforce es6 style
    
    * style(es6): use es6 destructors
    
    * chore(ci): remove circle-ci and codecov
    
    Eliminate redundant CI tasks, since we use travis-ci and coveralls, already
    
    #13
    
    * test(crc-reports): add formatter
    
    #1
    
    * docs(README): clean up badges
    
    #18
    
    * chore(ci): remove redundant services
    
    * Coverity (appears to be a paid service, now)
    * Snyk (this is covered by Bithound)
    
    #13
    
    * chore(sonarqube): add regex for all branches
    
    * Evaluate all branches that conform to the Angular preset for conventional-changelog
    * See https://git.io/vSppK for valid branch (and commit) prefixes
    
    #13
    
    * chore(travis-ci): add regex delimiters
    
    #13
    
    * docs(README): remove sonar "develop" branch from badge
    
    
    #13
    
    * test(cli): ignore branch
    
    #13
    
    * chore(package): resolve vulnerabilities
    
    * Quoteless Attributes in Templates can lead to Content Injection (handlebars@3.0.3)
    * Incorrect Handling of Non-Boolean Comparisons During Minification (uglify-js@2.3.6)
    * Regular Expression Denial of Service (uglify-js@2.3.6)
    
    #13
    
    * chore(lint): replace
    
    * function expressions
    * let with const (where applicable)
    
    #13, #30
    
    * chore(lint): replace
    
    * let with const (where applicable)
    * functions declarations with expressions
    * allow console statements for Node.js CLI
    
    #13, #30
    
    * chore(packages): update dependencies
    
    * Remove shebang-loader from devDependencies
    * Shrinkwrap
    * Update nested packages with vulnerabilities
    
    #13, #30
    
    * chore(lint): ignore "Unexpected block statement surrounding arrow body."
    
    #13, #30
    
    * chore(package): update isarray to latest version
    
    Cannot remove this nested dependency even though it's deprecated.
    
    #13, #30
    
    * chore(lint): resolve "Unexpected block statement surrounding arrow body."
    
    #13, #30
    
    * chore(lint): giving up on "Unexpected block statement surrounding arrow body" for now.
    
    #13, #30
    
    * chore(lint): resolve "Unexpected block statement surrounding arrow body."
    
    #13, #30

--- tests/crc-model-formatter.spec.js
+++ tests/crc-model-formatter.spec.js
@@ -4,7 +4,7 @@ const relativePath = require('relative-path');
 const chai = require('chai');
 const dirtyChai = require('dirty-chai');
 const {expect} = chai;
-const _ = require('lodash');
+const { forEach, map } = require('lodash');
 const fs = require('fs');
 const path = require('path');
 const codeFixturePath = './fixtures/es5-object-identification.js';
@@ -13,10 +13,10 @@ const CrcModelList = require('../lib/crc-model-list');
 
 chai.use(dirtyChai);
 
-describe('CrcModelFormatter', function () {
+describe('CrcModelFormatter', () => {
     let code, crcModelList, formatter, libFilePath, template;
 
-    beforeEach(function () {
+    beforeEach(() => {
         libFilePath = relativePath(codeFixturePath);
         code = fs.readFileSync(libFilePath);
         libFilePath = path.join(__dirname, '../lib/templates/crc-card.html');
@@ -25,9 +25,9 @@ describe('CrcModelFormatter', function () {
         formatter = new CrcModelFormatter(template);
     });
 
-    afterEach(function () {
+    afterEach(() => {
         template = null;
-        _.map(crcModelList.models, function (model) {
+        map(crcModelList.models, (model) => {
             model.responsibilities = null;
         });
         crcModelList.models = null;
@@ -35,20 +35,18 @@ describe('CrcModelFormatter', function () {
         formatter = null;
     });
 
-    it('takes a template string', function () {
+    it('takes a template string', () => {
         expect(formatter.template).to.exist();
         formatter = new CrcModelFormatter();
         expect(formatter.template).not.to.exist();
     });
 
-    it('formats an CrcModelList as an HTML/markdown-friendly report of CRC "cards"', function () {
-        let report;
-
+    it('formats an CrcModelList as an HTML/markdown-friendly report of CRC "cards"', () => {
         const loadResponsibilities = (letters) => {
             const info = 'Disambiguation for the letter ';
             const action = 'Clarifies pronunciation when spelling with the letter ';
-            _.forEach(letters, function (letter, idx) {
-                let faa = '"' + letter + '"';
+            forEach(letters, (letter, idx) => {
+                const faa = '"' + letter + '"';
                 crcModelList.models[idx].responsibilities.push(info + faa);
                 crcModelList.models[idx].responsibilities.push(action + faa);
             });
@@ -62,10 +60,9 @@ describe('CrcModelFormatter', function () {
             'E',
             'F'
         ]);
-        report = formatter.format(crcModelList);
+        const report = formatter.format(crcModelList);
         expect(report).to.exist();
         expect(report.length).to.be.at.least(10);
-        //Console.log(report);
     });
 
 });
--- tests/crc-model.spec.js
+++ tests/crc-model.spec.js
@@ -8,10 +8,10 @@ const CrcModel = require('../lib/crc-model');
 
 chai.use(dirtyChai);
 
-describe('CrcModels represent objects\' behaviors and dependencies. A CrcModel', function () {
+describe('CrcModels represent objects\' behaviors and dependencies. A CrcModel', () => {
 
-    it('is identifiable by the object\'s name', function () {
-        let crc = new CrcModel('User');
+    it('is identifiable by the object\'s name', () => {
+        const crc = new CrcModel('User');
         expect(crc.name).to.exist();
         expect(crc.name).to.equal('User');
         expect(crc.responsibilities).to.be.empty();
@@ -21,26 +21,26 @@ describe('CrcModels represent objects\' behaviors and dependencies. A CrcModel',
         expect(crc.declaration).to.be.null();
     });
 
-    it('must have a name, or an error will be thrown', function () {
-        let fn = function () {
+    it('must have a name, or an error will be thrown', () => {
+        const fn = () => {
             const crc = new CrcModel();
             expect(crc).not.to.be.defined();
         };
         expect(fn).to.throw(TypeError);
     });
 
-    it('lists the object\'s responsibilities', function () {
-        let verification = 'Verifies user-agent identity';
-        let crc = new CrcModel('Authenticator', {responsibilities: verification});
+    it('lists the object\'s responsibilities', () => {
+        const verification = 'Verifies user-agent identity';
+        const crc = new CrcModel('Authenticator', {responsibilities: verification});
         expect(crc.responsibilities).to.contain(verification);
     });
 
-    it('lists other objects that it collaborates with', function () {
-        let options = {
+    it('lists other objects that it collaborates with', () => {
+        const options = {
             responsibilities: ['Verifies user-agent identity'],
             collaborators   : [new CrcModel('WebForm')]
         };
-        let crc = new CrcModel('Authenticator', options);
+        const crc = new CrcModel('Authenticator', options);
         expect(_.first(crc.collaborators)).to.have.all.keys(
             'name',
             'responsibilities',
@@ -53,9 +53,9 @@ describe('CrcModels represent objects\' behaviors and dependencies. A CrcModel',
         );
     });
 
-    it('provides an abstract syntax tree (AST) for static code analysis', function () {
-        let ast = JSON.parse('{"type":"MockDeclaration","start":0,"end":14,"range":[0,14]}');
-        let options = {
+    it('provides an abstract syntax tree (AST) for static code analysis', () => {
+        const ast = JSON.parse('{"type":"MockDeclaration","start":0,"end":14,"range":[0,14]}');
+        const options = {
             responsibilities: [],
             collaborators   : [],
             references      : {
@@ -65,22 +65,21 @@ describe('CrcModels represent objects\' behaviors and dependencies. A CrcModel',
             declaration     : {},
             superClass      : Object
         };
-        let crc = new CrcModel('BloatedObject', options);
+        const crc = new CrcModel('BloatedObject', options);
         expect(crc.identifier).to.exist();
         expect(crc.references).not.to.be.empty();
 
     });
 
-    specify('CRC models should not share arrays by reference', function () {
-        let crc1, crc2;
-        crc1 = new CrcModel('crcModelOne', {
+    specify('CRC models should not share arrays by reference', () => {
+        const crc1 = new CrcModel('crcModelOne', {
             responsibilities: [
                 1,
                 2,
                 3
             ]
         });
-        crc2 = new CrcModel('crcModelTwo', {
+        const crc2 = new CrcModel('crcModelTwo', {
             responsibilities: [
                 1,
                 2,
@@ -94,7 +93,7 @@ describe('CrcModels represent objects\' behaviors and dependencies. A CrcModel',
     });
 
     it('declares its prototype', () => {
-        let crc = new CrcModel('User');
+        const crc = new CrcModel('User');
         expect(crc.superClass).to.equal(Object);
     });
 

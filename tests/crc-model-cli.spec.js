940032b110a5ed8b0155b5dfbb8c6c7fb042c627
Greg Swindle <greg@swindle.net>
2017-06-27 22:25:35 -0500

    style(lint): fix errors
    * arrow-body-style
    * semi
    * indent
    
    #54,#55,#56,#57

--- tests/crc-model-cli.spec.js
+++ tests/crc-model-cli.spec.js
@@ -14,48 +14,48 @@ chai.use(dirtychai);
 
 describe('crc-model-cli is a command line program, that,', () => {
 
-  beforeEach(() => {
-    concat = sinon.spy(concat);
-    open = sinon.stub(fs, 'open').callsFake(noop);
-  });
-
-  afterEach(() => {
-    open = null;
-    sinon.restore();
-  });
-
-  describe('when given a glob pattern,', () => {
-    it('concatenates the files into a single object for AST evaluation');
-
-    it('writes to the console on error', () => {
-      // const error = sinon.stub(console, 'error').callsFake(noop);
-      cli.parse('@#@$%#*)@)!&&&.bad');
-      expect(console.error.called).to.be.false();
+    beforeEach(() => {
+        concat = sinon.spy(concat);
+        open = sinon.stub(fs, 'open').callsFake(noop);
     });
-  });
 
-  describe('when given a file with a shebang (#!),', () => {
-    it('will comment the shebang to prevent AST errors', () => {
-      expect(() => {
-        cli.parse('./fixtures/cli-shebang.js');
-      }).not.to.throw(Error);
+    afterEach(() => {
+        open = null;
+        sinon.restore();
     });
-  });
 
-  describe('when given a path to a file,', () => {
-    it('creates a report in that location', () => {
-      // const parse = sinon.stub(cli, 'parse').callsFake(noop);
-      cli.parse('./fixtures/cli-shebang.js, -o ./reports/stub-report.md');
-      console.log(cli.rawArgs);
-      expect(cli.rawArgs).to.contain('./fixtures/cli-shebang.js');
+    describe('when given a glob pattern,', () => {
+        it('concatenates the files into a single object for AST evaluation');
+
+        it('writes to the console on error', () => {
+            sinon.stub(console, 'error').callsFake(noop);
+            cli.parse('@#@$%#*)@)!&&&.bad');
+            expect(console.error.called).to.be.false();
+        });
     });
-  });
 
-  describe('when a file path is omitted,', () => {
-    it('saves a report in the default location "./reports/crc-model-report.md"', () => {
-      cli.parse('');
-
-      expect(cli.rawArgs).to.equal('');
+    describe('when given a file with a shebang (#!),', () => {
+        it('will comment the shebang to prevent AST errors', () => {
+            expect(() => {
+                cli.parse('./fixtures/cli-shebang.js');
+            }).not.to.throw(Error);
+        });
     });
-  });
+
+    describe('when given a path to a file,', () => {
+        it('creates a report in that location', () => {
+      // Const parse = sinon.stub(cli, 'parse').callsFake(noop);
+            cli.parse('./fixtures/cli-shebang.js, -o ./reports/stub-report.md');
+            console.log(cli.rawArgs);
+            expect(cli.rawArgs).to.contain('./fixtures/cli-shebang.js');
+        });
+    });
+
+    describe('when a file path is omitted,', () => {
+        it('saves a report in the default location "./reports/crc-model-report.md"', () => {
+            cli.parse('');
+
+            expect(cli.rawArgs).to.equal('');
+        });
+    });
 });

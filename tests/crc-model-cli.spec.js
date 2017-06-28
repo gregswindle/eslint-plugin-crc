b1d95f971f921f479c4582c927274c1cf518c682
Greg Swindle <greg@swindle.net>
2017-06-27 22:13:28 -0500

    style(lint): fix errors
    * no-unsed-var
    * no-console
    
    #54,#55,#56,#57

--- tests/crc-model-cli.spec.js
+++ tests/crc-model-cli.spec.js
@@ -1,4 +1,4 @@
-
+/*eslint no-console: "off"*/
 const chai = require('chai');
 const cli = require('../lib/crc-reporter');
 let concat = require('concat');
@@ -6,57 +6,56 @@ const dirtychai = require('dirty-chai');
 const fs = require('fs');
 const noop = () => {};
 const sinon = require('sinon').sandbox.create();
-const { expect } = chai;
+const {
+  expect
+} = chai;
 
 chai.use(dirtychai);
 
 describe('crc-model-cli is a command line program, that,', () => {
-    let open = null;
 
-    beforeEach(() => {
-        concat = sinon.spy(concat);
-        open = sinon.stub(fs, 'open').callsFake(noop);
+  beforeEach(() => {
+    concat = sinon.spy(concat);
+    open = sinon.stub(fs, 'open').callsFake(noop);
+  });
+
+  afterEach(() => {
+    open = null;
+    sinon.restore();
+  });
+
+  describe('when given a glob pattern,', () => {
+    it('concatenates the files into a single object for AST evaluation');
+
+    it('writes to the console on error', () => {
+      // const error = sinon.stub(console, 'error').callsFake(noop);
+      cli.parse('@#@$%#*)@)!&&&.bad');
+      expect(console.error.called).to.be.false();
     });
+  });
 
-    afterEach(() => {
-        open = null;
-        sinon.restore();
+  describe('when given a file with a shebang (#!),', () => {
+    it('will comment the shebang to prevent AST errors', () => {
+      expect(() => {
+        cli.parse('./fixtures/cli-shebang.js');
+      }).not.to.throw(Error);
     });
+  });
 
-    describe('when given a glob pattern,', () => {
-        it('concatenates the files into a single object for AST evaluation', () => {
-            console.log('waiting for a spec');
-        });
-
-        it('writes to the console on error', () => {
-            const error = sinon.stub(console, 'error').callsFake(noop);
-            cli.parse('@#@$%#*)@)!&&&.bad');
-            expect(console.error.called).to.be.false();
-        });
+  describe('when given a path to a file,', () => {
+    it('creates a report in that location', () => {
+      // const parse = sinon.stub(cli, 'parse').callsFake(noop);
+      cli.parse('./fixtures/cli-shebang.js, -o ./reports/stub-report.md');
+      console.log(cli.rawArgs);
+      expect(cli.rawArgs).to.contain('./fixtures/cli-shebang.js');
     });
+  });
 
-    describe('when given a file with a shebang (#!),', () => {
-        it('will comment the shebang to prevent AST errors', () => {
-            expect(() => {
-                cli.parse('./fixtures/cli-shebang.js');
-            }).not.to.throw(Error);
-        });
+  describe('when a file path is omitted,', () => {
+    it('saves a report in the default location "./reports/crc-model-report.md"', () => {
+      cli.parse('');
+
+      expect(cli.rawArgs).to.equal('');
     });
-
-    describe('when given a path to a file,', () => {
-        it('creates a report in that location', () => {
-            const parse = sinon.stub(cli, 'parse').callsFake(noop);
-            cli.parse('./fixtures/cli-shebang.js, -o ./reports/stub-report.md');
-            console.log(cli.rawArgs);
-            expect(cli.rawArgs).to.contain('./fixtures/cli-shebang.js');
-        });
-    });
-
-    describe('when a file path is omitted,', () => {
-        it('saves a report in the default location "./reports/crc-model-report.md"', () => {
-            cli.parse('');
-
-            expect(cli.rawArgs).to.equal('');
-        });
-    });
+  });
 });

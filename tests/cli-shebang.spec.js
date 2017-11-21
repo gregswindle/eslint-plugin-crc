b6a16d14df5e3ce045a117d216291fb65fe46d84
Greg Swindle <greg@swindle.net>
2017-11-20 20:35:21 -0600

    refactor(inheritance): simplify inheritance discovery
    #41

--- tests/cli-shebang.spec.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node --harmony
-module.exports = {
-    cliShebang: {
-        spec: {
-            passed: true
-        }
-    }
-};
--- tests/lib/rules/generate-crc.js
+++ tests/lib/rules/generate-crc.js
@@ -8,30 +8,40 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/generate-crc"),
+const rule = require("../../../lib/rules/generate-crc"),
 
     RuleTester = require("eslint").RuleTester;
 
+    RuleTester.setDefaultConfig({
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module"
+      }
+    })
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
+const ruleTester = new RuleTester();
+const invalidPattern = {
+    code: "",
+    errors: [{
+        message: "Fill me in.",
+        type: "Me too"
+    }]
+}
 ruleTester.run("generate-crc", rule, {
 
     valid: [
-
         // give me some code that won't trigger a warning
+        'Square.prototype = new Polygon()',
+        'Square.prototype = Object.create(Polygon.prototype)',
+        'Square.prototype.constructor = Polygon',
+        'class Square extends Polygon {}'
     ],
 
     invalid: [
-        {
-            code: "",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
+
     ]
 });

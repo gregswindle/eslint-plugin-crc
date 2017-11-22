29627f72ac7f680533a66f6c4117529030404f7c
Greg Swindle <greg@swindle.net>
2017-11-22 07:05:25 -0600

    style(lint): fix formatting
    Add properties-parser as a devDependency, too.
    
    #41,#110

--- .ci/bump/index.js
+++ .ci/bump/index.js
@@ -1,12 +1,13 @@
-
-const {version} = require("../../package");
+// eslint-disable-next-line node/no-unsupported-features
+const { version } = require("../../package");
 const path = require("path");
+// eslint-disable-next-line node/no-unpublished-require
 const prop = require("properties-parser");
 
 const bumpSonarProjectVersion = () => {
-    const sonarPropertiesEditor = prop.createEditor(path.resolve(__dirname, "../../sonar-project.properties"));
-    sonarPropertiesEditor.set("sonar.projectVersion", version);
-    sonarPropertiesEditor.save();
+  const sonarPropertiesEditor = prop.createEditor(path.resolve(__dirname, "../../sonar-project.properties"));
+  sonarPropertiesEditor.set("sonar.projectVersion", version);
+  sonarPropertiesEditor.save();
 };
 
 bumpSonarProjectVersion();

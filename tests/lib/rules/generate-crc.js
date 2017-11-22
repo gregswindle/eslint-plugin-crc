5c317c04f0626fe6c081cf12a67d05784db7c108
Greg Swindle <greg@swindle.net>
2017-11-21 22:01:46 -0600

    style(eslint): fix lint errors
    #41,#110

--- tests/lib/rules/generate-crc.js
+++ tests/lib/rules/generate-crc.js
@@ -7,37 +7,36 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/generate-crc"),
+const { RuleTester } = require("eslint");
+const rule = require("../../../lib/rules/generate-crc");
 
-    RuleTester = require("eslint").RuleTester;
-
-    RuleTester.setDefaultConfig({
-      parserOptions: {
+RuleTester.setDefaultConfig({
+    parserOptions: {
         ecmaVersion: 6,
         sourceType: "module"
-      }
-    })
+    }
+});
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
 const ruleTester = new RuleTester();
-const invalidPattern = {
-    code: "",
-    errors: [{
-        message: "Fill me in.",
-        type: "Me too"
-    }]
-}
+// Const invalidPattern = {
+//     Code: "",
+//     Errors: [{
+//         Message: "Fill me in.",
+//         Type: "Me too"
+//     }]
+// };
 ruleTester.run("generate-crc", rule, {
 
     valid: [
-        // give me some code that won't trigger a warning
-        'Square.prototype = new Polygon()',
-        'Square.prototype = Object.create(Polygon.prototype)',
-        'Square.prototype.constructor = Polygon',
-        'class Square extends Polygon {}'
+        // Give me some code that won't trigger a warning
+        "Square.prototype = new Polygon()",
+        "Square.prototype = Object.create(Polygon.prototype)",
+        "Square.prototype.constructor = Polygon",
+        "class Square extends Polygon {}"
     ],
 
     invalid: [

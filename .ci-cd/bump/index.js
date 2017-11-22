a38a7db9783a1e2a93a473e5836d7380fb0a1c32
Greg Swindle <greg@swindle.net>
2017-11-21 22:38:25 -0600

    style(lint): fix lint errors
    #41,#110

--- .ci-cd/bump/index.js
+++ .ci-cd/bump/index.js
@@ -1,11 +1,11 @@
 
-const {version} = require('../../package');
-const path = require('path');
-const prop = require('properties-parser');
+const {version} = require("../../package");
+const path = require("path");
+const prop = require("properties-parser");
 
 const bumpSonarProjectVersion = () => {
-    const sonarPropertiesEditor = prop.createEditor(path.resolve(__dirname, '../../sonar-project.properties'));
-    sonarPropertiesEditor.set('sonar.projectVersion', version);
+    const sonarPropertiesEditor = prop.createEditor(path.resolve(__dirname, "../../sonar-project.properties"));
+    sonarPropertiesEditor.set("sonar.projectVersion", version);
     sonarPropertiesEditor.save();
 };
 

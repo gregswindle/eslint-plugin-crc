079405137a5b2ce16b9755bd6d2e72d4347aa744
Greg Swindle <greg@swindle.net>
2017-12-08 21:03:14 -0600

    ci(windows): change server settings
    #41,#110

--- tests/lib/crc/descriptors/class-declaration-crc-class.spec.js
+++ tests/lib/crc/descriptors/class-declaration-crc-class.spec.js
@@ -9,7 +9,6 @@ describe("ClassDeclarationCrcClass", () => {
     it("creates a CrcClass from context", () => {
       const crcClass = ClassDeclarationCrcClass.factory(context);
 
-      // TODO: ensure that context provides one class at a time!
       expect(crcClass.name).to.equal("Polygon");
       expect(crcClass.superClass).to.be.null;
       expect(crcClass.code).to.equal(context.code);
